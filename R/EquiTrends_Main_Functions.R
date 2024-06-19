176403a617c3968f24a3cbac9a40a86a6ffa11dd
TiesBos <ties.bos2002@gmail.com>
2024-06-19 12:43:31 +0200

    input changed to better format

--- R/EquiTrends_Main_Functions.R
+++ R/EquiTrends_Main_Functions.R
@@ -8,9 +8,9 @@
 #' @param period If 'data' is supplied, a scalar identifying the column number or column-name character string associated with period (time) data. The time variable has to be numeric. If 'data' is not supplied, a numeric vector (of the same dimension as Y) indicating time.
 #' @param X  If 'data' is supplied, a vector of column numbers or column-name character strings that identifies the control variables’ columns. If data is not supplied, a vector, matrix or data.frame containing the control variables.
 #' @param data An optional data.frame object containing the variables in Y, ID, G, T and, if supplied, X and cluster as its columns.
-#' @param delta The scalar equivalence threshold (must be positive). The default is NULL, implying that the function must look for the minimum value for which the null of ”non-negligible differences” can still be rejected.
-#' @param pretreatment.period A numeric vector identifying the pre-treatment periods that should be used for testing. The default is to use all periods that are included in T.
-#' @param base.period The pre-treatment period to compare the post-treatment observation to. The default is to take the last specified pre-treatment period.
+#' @param equiv_threshold The scalar equivalence threshold (must be positive). The default is NULL, implying that the function must look for the minimum value for which the null of ”non-negligible differences” can be rejected.
+#' @param pretreatment_period A numeric vector identifying the pre-treatment periods that should be used for testing. The default is to use all periods that are included in T.
+#' @param base_period The pre-treatment period to compare the post-treatment observation to. The default is to take the last specified pre-treatment period.
 #' @param vcov The variance-covariance matrix that needs to be used. See details for more details.
 #' @param cluster If vcov = "CL", a vector indicating which observations belong to the same cluster of the same length as Y. If 'data' is supplied, 'cluster' must be either the column index or column name of this vector in the data.frame/matrix. The default (cluster=NULL) assumes every unit in ID is its own cluster.
 #' @param alpha Significance level of the test. The default is 0.05.
@@ -21,9 +21,9 @@
 #'
 #' @return hoi
 #' @export
-#'
-maxEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, delta = NULL,  
-                       pretreatment.period = NULL, base.period = NULL, 
+#' 
+maxEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, equiv_threshold = NULL,  
+                       pretreatment_period = NULL, base_period = NULL, 
                        vcov = NULL, cluster = NULL, alpha = 0.05, 
                        type = c("IU", "Boot", "Wild"), B = 1000){
   # If no type is specified, the type is "IU"
@@ -34,34 +34,34 @@ maxEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, delta = NULL,
   
   # Error/Warnings:
   # Checking errors specific to this test procedure
-  error.maxTest <- maxTest.error(type, delta, vcov)
-  if(error.maxTest$error){stop(error.maxTest$message)}
+  error_maxTest <- maxTest.error(type, equiv_threshold, vcov)
+  if(error_maxTest$error){stop(error_maxTest$message)}
   
   # General error checking:
-  error.test <- EquiTrends.inputcheck(Y, ID, G, period, X, data, delta, pretreatment.period, 
-                                     base.period, cluster, alpha)
+  error_test <- EquiTrends.inputcheck(Y, ID, G, period, X, data, equiv_threshold, pretreatment_period, 
+                                     base_period, cluster, alpha)
   
-  if(error.test$error){stop(error.test$message)}
+  if(error_test$error){stop(error_test$message)}
   
   # Structuring the data:
-  data.constr <- EquiTrends.dataconstr(Y, ID, G, period, X, data, pretreatment.period, base.period,
+  data_constr <- EquiTrends.dataconstr(Y, ID, G, period, X, data, pretreatment_period, base_period,
                                    cluster)
   # The dataframe:
-  df <- data.constr$dataset
+  df <- data_constr$dataset
   
   # The base period:
-  base.period <- data.constr$baseperiod
+  base_period <- data_constr$baseperiod
   
   # Number of individuals
   N <- length(df[unique(df$ID), "ID"])
   # Number of Time Periods:
-  no.periods <- length(df[unique(df$period), "period"])
+  no_periods <- length(df[unique(df$period), "period"])
   
   if(type=="IU"){
-    test_results <- maxTestIU(df, delta, vcov, cluster, alpha, N, no.periods, base.period)
+    test_results <- maxTestIU(df, equiv_threshold, vcov, cluster, alpha, N, no_periods, base_period)
     class(test_results) <- "maxEquivTestIU"
   } else if (type == "Boot" || type == "Wild"){
-    test_results <- maxTestBoot(df, delta, alpha, N, B, no.periods, base.period, type)
+    test_results <- maxTestBoot(df, equiv_threshold, alpha, N, B, no_periods, base_period, type)
     class(test_results) <- "maxEquivTestBoot"
   }
   
@@ -78,9 +78,9 @@ maxEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, delta = NULL,
 #' @param period If 'data' is supplied, a scalar identifying the column number or column-name character string associated with period (time) data. The time variable has to be numeric. If 'data' is not supplied, a numeric vector (of the same dimension as Y) indicating time.
 #' @param X  If 'data' is supplied, a vector of column numbers or column-name character strings that identifies the control variables’ columns. If data is not supplied, a vector, matrix or data.frame containing the control variables.
 #' @param data An optional data.frame object containing the variables in Y, ID, G, T and, if supplied, X and cluster as its columns.
-#' @param delta The scalar equivalence threshold (must be positive). The default is NULL, implying that the function must look for the minimum value for which the null of ”non-negligible differences” can still be rejected.
-#' @param pretreatment.period A numeric vector identifying the pre-treatment periods that should be used for testing. The default is to use all periods that are included in T.
-#' @param base.period The pre-treatment period to compare the post-treatment observation to. The default is to take the last specified pre-treatment period.
+#' @param equiv_threshold The scalar equivalence threshold (must be positive). The default is NULL, implying that the function must look for the minimum value for which the null of ”non-negligible differences” can still be rejected.
+#' @param pretreatment_period A numeric vector identifying the pre-treatment periods that should be used for testing. The default is to use all periods that are included in T.
+#' @param base_period The pre-treatment period to compare the post-treatment observation to. The default is to take the last specified pre-treatment period.
 #' @param vcov The variance-covariance matrix that needs to be used. See details for more details.
 #' @param cluster If vcov = "CL", a vector indicating which observations belong to the same cluster of the same length as Y. If 'data' is supplied, 'cluster' must be either the column index or column name of this vector in the data.frame/matrix. The default (cluster=NULL) assumes every unit in ID is its own cluster.
 #' @param alpha Significance level of the test. The default is 0.05.
@@ -88,32 +88,32 @@ maxEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, delta = NULL,
 #' @return hoi
 #' @export
 #'
-meanEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, delta = NULL,  
-                     pretreatment.period = NULL, base.period = NULL, 
+meanEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, equiv_threshold = NULL,  
+                     pretreatment_period = NULL, base_period = NULL, 
                      vcov = NULL, cluster = NULL, alpha=0.05){
   
   # Error/Warnings:
   # General error checking:
-  error.test <- EquiTrends.inputcheck(Y, ID, G, period, X, data, delta, pretreatment.period, 
-                                     base.period, cluster, alpha)
+  error_test <- EquiTrends.inputcheck(Y, ID, G, period, X, data, equiv_threshold, pretreatment_period, 
+                                     base_period, cluster, alpha)
   
-  if(error.test$error){stop(error.test$message)}
+  if(error_test$error){stop(error_test$message)}
   
   # We construct the data.frame:
-  data.constr <- EquiTrends.dataconstr(Y, ID, G, period, X, data, pretreatment.period, base.period,
+  data_constr <- EquiTrends.dataconstr(Y, ID, G, period, X, data, pretreatment_period, base_period,
                                    cluster)
-  df <- data.constr$data
+  df <- data_constr$data
   
   # Store the base.period:
-  base.period <- data.constr$baseperiod
+  base.period <- data_constr$baseperiod
   
   # number of individuals in the sample:
   N <- length(df[unique(df$ID), "ID"])
   # The number of periods:
-  no.periods <- length(unique(df$period))
+  no_periods <- length(unique(df$period))
   
   # Perform the test:
-  results <- meanTest.func(df, delta, vcov, cluster, alpha, N, no.periods, base.period)
+  results <- meanTest.func(df, equiv_threshold, vcov, cluster, alpha, N, no_periods, base_period)
   
   # give it the specified class:
   class(results) <- "meanEquivTest"
@@ -130,40 +130,40 @@ meanEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, delta = NULL,
 #' @param period If 'data' is supplied, a scalar identifying the column number or column-name character string associated with period (time) data. The time variable has to be numeric. If 'data' is not supplied, a numeric vector (of the same dimension as Y) indicating time.
 #' @param X  If 'data' is supplied, a vector of column numbers or column-name character strings that identifies the control variables’ columns. If data is not supplied, a vector, matrix or data.frame containing the control variables.
 #' @param data An optional data.frame object containing the variables in Y, ID, G, T and, if supplied, X and cluster as its columns.
-#' @param delta The scalar equivalence threshold (must be positive). The default is NULL, implying that the function must look for the minimum value for which the null of ”non-negligible differences” can still be rejected.
-#' @param pretreatment.period A numeric vector identifying the pre-treatment periods that should be used for testing. The default is to use all periods that are included in T.
-#' @param base.period The pre-treatment period to compare the post-treatment observation to. The default is to take the last specified pre-treatment period.
-#' @param no.lambda The scaling factor of the uniform distribution
+#' @param equiv_threshold The scalar equivalence threshold (must be positive). The default is NULL, implying that the function must look for the minimum value for which the null of ”non-negligible differences” can still be rejected.
+#' @param pretreatment_period A numeric vector identifying the pre-treatment periods that should be used for testing. The default is to use all periods that are included in T.
+#' @param base_period The pre-treatment period to compare the post-treatment observation to. The default is to take the last specified pre-treatment period.
+#' @param no_lambda The scaling factor of the uniform distribution
 #' @param alpha Significance level of the test. Must be one of the following: 0.01, 0.025, 0.05, 0.1 or 0.2. The default is 0.05.
 #'
 #' @return hoi
 #' @export
 #'
 #' 
-rmsEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, delta = NULL,  
-                    pretreatment.period = NULL, base.period = NULL, 
-                    alpha=0.05, no.lambda = 5){
+rmsEquivTest <- function(Y, ID, G, period, X = NULL, data = NULL, equiv_threshold = NULL,  
+                    pretreatment_period = NULL, base_period = NULL, 
+                    alpha=0.05, no_lambda = 5){
   
   # rmsTest specific error checking:
-  error.rmsTest <- rmsTest.error(alpha, no.lambda)
-  if(error.rmsTest$error){stop(error.rmsTest$message) }
+  error_rmsTest <- rmsTest.error(alpha, no_lambda)
+  if(error_rmsTest$error){stop(error_rmsTest$message) }
   
   # General error checking:
-  error.test <- EquiTrends.inputcheck(Y, ID, G, period, X, data, delta, pretreatment.period, 
-                                     base.period, cluster = NULL, alpha)
+  error_test <- EquiTrends.inputcheck(Y, ID, G, period, X, data, equiv_threshold, pretreatment_period, 
+                                     base_period, cluster = NULL, alpha)
   
-  if(error.test$error){stop(error.test$message)}
+  if(error_test$error){stop(error_test$message)}
   
   # Read the data:
-  data.constr <- EquiTrends.dataconstr(Y, ID, G, period, X, data, pretreatment.period, 
-                                       base.period, cluster=NULL)
-  df <- data.constr$dataset
-  base.period <- data.constr$baseperiod
+  data_constr <- EquiTrends.dataconstr(Y, ID, G, period, X, data, pretreatment_period, 
+                                       base_period, cluster=NULL)
+  df <- data_constr$dataset
+  base.period <- data_constr$baseperiod
   
   # Perform the test:
-  test.results <- rmsTest.func(df, delta, alpha, no.lambda, base.period)
+  test_results <- rmsTest.func(df, equiv_threshold, alpha, no_lambda, base_period)
   
-  return(test.results)
+  return(test_results)
 }
 
 
