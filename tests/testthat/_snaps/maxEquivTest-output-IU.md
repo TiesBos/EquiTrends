1189667cdf7c00f9ef4316704f25394521d20eb2
TiesBos <ties.bos2002@gmail.com>
2024-08-22 12:53:16 +0200

    testthat update

--- tests/testthat/_snaps/maxEquivTest-output-IU.md
+++ tests/testthat/_snaps/maxEquivTest-output-IU.md
@@ -1,4 +1,4 @@
-# maxEquivTest return for matrix input and standard variance-covariance matrix
+# maxEquivTest print for standard variance-covariance matrix
 
     Code
       print(maxEquivTest_results)
@@ -8,15 +8,15 @@
                      Equivalence Tests for Pre-trends in DiD Estimation
                      ==================================================
       Type: Intersection Union 
-      Significance level: 0.1 
+      Significance level: 0.05 
       Alternative hypothesis: the maximum placebo effect does not exceed the equivalence threshold.
-      Minimum equivalence threshold to accept the alternative: 0.3899 
+      Minimum equivalence threshold to accept the alternative: 0.4345 
       ---
        Estimate	Std. Error 	 Minimum Equivalence Threshold 
-      0.1151   	0.1229   	2.721e-01    
-      0.1367   	0.1229   	2.940e-01    
-      0.2324   	0.1229   	3.899e-01    
-      0.0120   	0.1229   	1.150280e-09 
+      0.1151   	0.1229   	0.3171    
+      0.1367   	0.1229   	0.3388    
+      0.2324   	0.1229   	0.4345    
+      0.0120   	0.1229   	0.1157    
       ---
       No. placebo coefficients estimated: 4 
       Base period: 5 
@@ -39,13 +39,13 @@
       Type: Intersection Union 
       Alternative hypothesis: the maximum placebo effect does not exceed the equivalence threshold of 0.2 .
       Reject null hypothesis: FALSE 
-      ( Critical values are printed for the significance level: 0.1 )
+      ( Critical values are printed for the significance level: 0.05 )
       ---
       Abs. Estimate	Std. Error	Critical Value 
-      0.1151       	0.1229       	0.05496       
-      0.1367       	0.1229       	0.05498       
-      0.2324       	0.1229       	0.05498       
-      0.0120       	0.1229       	0.05498       
+      0.1151       	0.1229       	0.02852       
+      0.1367       	0.1229       	0.02853       
+      0.2324       	0.1229       	0.02853       
+      0.0120       	0.1229       	0.02853       
       ---
       No. placebo coefficients estimated: 4 
       Base period: 5 
@@ -66,15 +66,15 @@
                      Equivalence Tests for Pre-trends in DiD Estimation
                      ==================================================
       Type: Intersection Union 
-      Significance level: 0.1 
+      Significance level: 0.05 
       Alternative hypothesis: the maximum placebo effect does not exceed the equivalence threshold.
-      Minimum equivalence threshold to accept the alternative: 0.3899 
+      Minimum equivalence threshold to accept the alternative: 0.4345 
       ---
        Estimate	Std. Error 	 Minimum Equivalence Threshold 
-      0.1151   	0.1229   	2.721e-01    
-      0.1367   	0.1229   	2.940e-01    
-      0.2324   	0.1229   	3.899e-01    
-      0.0120   	0.1229   	1.150280e-09 
+      0.1151   	0.1229   	0.3171    
+      0.1367   	0.1229   	0.3388    
+      0.2324   	0.1229   	0.4345    
+      0.0120   	0.1229   	0.1157    
       ---
       No. placebo coefficients estimated: 4 
       Base period: 5 
@@ -97,13 +97,13 @@
       Type: Intersection Union 
       Alternative hypothesis: the maximum placebo effect does not exceed the equivalence threshold of 0.2 .
       Reject null hypothesis: FALSE 
-      ( Critical values are printed for the significance level: 0.1 )
+      ( Critical values are printed for the significance level: 0.05 )
       ---
       Abs. Estimate	Std. Error	Critical Value 
-      0.1151       	0.1229       	0.05496       
-      0.1367       	0.1229       	0.05498       
-      0.2324       	0.1229       	0.05498       
-      0.0120       	0.1229       	0.05498       
+      0.1151       	0.1229       	0.02852       
+      0.1367       	0.1229       	0.02853       
+      0.2324       	0.1229       	0.02853       
+      0.0120       	0.1229       	0.02853       
       ---
       No. placebo coefficients estimated: 4 
       Base period: 5 
--- tests/testthat/test-maxEquivTest-output-IU.R
+++ tests/testthat/test-maxEquivTest-output-IU.R
@@ -50,7 +50,6 @@ test_that("maxEquivTest return for matrix input and standard variance-covariance
   expect_equal(length(maxEquivTest_results$placebo_coefficients), 4)
   expect_equal(maxEquivTest_results$placebo_coefficients, placebo_coefs)
   
-  expect_snapshot(print(maxEquivTest_results))
   
   # Equivalence threshold specified:
   maxEquivTest_results2 <- maxEquivTest(Y = Y_data, ID= ID_data, G = G_data, 
@@ -74,7 +73,6 @@ test_that("maxEquivTest return for matrix input and standard variance-covariance
   expect_equal(maxEquivTest_results2$placebo_coefficients, placebo_coefs)
   expect_equal(maxEquivTest_results2$reject_null_hypothesis, FALSE)
   
-  expect_snapshot(print(maxEquivTest_results2))
   
   
   # with index input:
@@ -86,7 +84,6 @@ test_that("maxEquivTest return for matrix input and standard variance-covariance
                                         cluster = 7, data = sim_data,
                                         alpha = alpha,
                                         type = "IU")
-  expect_snapshot(print(maxEquivTest_results3))
   
   # Equivalence threshold specified:
   maxEquivTest_results4 <- maxEquivTest(Y = 1, ID= 2, G = 4, 
@@ -99,8 +96,6 @@ test_that("maxEquivTest return for matrix input and standard variance-covariance
                                         type = "IU")
   expect_equal(maxEquivTest_results3, maxEquivTest_results)
   expect_equal(maxEquivTest_results4, maxEquivTest_results2)
-  expect_snapshot(print(maxEquivTest_results4))
-  
 })
 
 test_that("maxEquivTest return for HC-type variance-covariance matrix",{
@@ -492,7 +487,85 @@ test_that("maxEquivTest return for CL-type variance-covariance matrix with clust
 })
 
 
+# Print functions:
+test_that("maxEquivTest print for standard variance-covariance matrix",{
+  sim_data <- readRDS(test_path("fixtures", "test_data.rds"))
+  
+  # The data in vector/matrix form:
+  Y_data <- sim_data$Y
+  ID_data <- sim_data$ID
+  G_data <- sim_data$G
+  period_data <- sim_data$period
+  X_data <- sim_data[, c("X_1", "X_2")]
+  cluster_data <- sim_data$cluster
+  
+  # For the matrix input version of the function:
+  # - No equivalence threshold:
+  pre_treatment_period <- 1:5
+  base_period <- 5
+  alpha <- 0.05
+  
+  
+  # Do the procedure by hand for comparison:
+  subdata <- sim_data[,c("ID", "period", "Y", "placebo_1", "placebo_2", "placebo_3", "placebo_4", "X_1", "X_2")]
+  test_formula <- as.formula(Y ~ X_1 + X_2 + placebo_1 + placebo_2 + placebo_3 + placebo_4)
+  plm_test <- plm::plm(test_formula, data=subdata, effect="twoways", model="within", index=c("ID","period"))
+  placebo_coefs <- plm_test$coefficients[c("placebo_1", "placebo_2", "placebo_3", "placebo_4")]
+  
+  vcov_mat <- plm_test$vcov
+  subcov_mat <- vcov_mat[c("placebo_1", "placebo_2", "placebo_3", "placebo_4"), c("placebo_1", "placebo_2", "placebo_3", "placebo_4")]
+  beta_var <- diag(subcov_mat)
+  
+  # Calculating the standard errors
+  beta_se <- sqrt(beta_var)
+  
+  maxEquivTest_results <- maxEquivTest(Y = Y_data, ID= ID_data, G = G_data, 
+                                       period = period_data, X=X_data, 
+                                       equiv_threshold = NULL,
+                                       pretreatment_period = pre_treatment_period, 
+                                       base_period = base_period,
+                                       cluster = cluster_data, 
+                                       alpha = alpha,
+                                       type = "IU")
+  
+  expect_snapshot(print(maxEquivTest_results))
+  
+  # Equivalence threshold specified:
+  maxEquivTest_results2 <- maxEquivTest(Y = Y_data, ID= ID_data, G = G_data, 
+                                        period = period_data, X=X_data, 
+                                        equiv_threshold = 0.2,
+                                        pretreatment_period = pre_treatment_period, 
+                                        base_period = base_period,
+                                        cluster = cluster_data, 
+                                        alpha = alpha,
+                                        type = "IU")
+  
+  expect_snapshot(print(maxEquivTest_results2))
+  
+  # with index input:
+  maxEquivTest_results3 <- maxEquivTest(Y = 1, ID= 2, G = 4, 
+                                        period = 3, X= c(5,6), 
+                                        equiv_threshold = NULL,
+                                        pretreatment_period = pre_treatment_period, 
+                                        base_period = base_period,
+                                        cluster = 7, data = sim_data,
+                                        alpha = alpha,
+                                        type = "IU")
+  expect_snapshot(print(maxEquivTest_results3))
+  
+  # Equivalence threshold specified:
+  maxEquivTest_results4 <- maxEquivTest(Y = 1, ID= 2, G = 4, 
+                                        period = 3, X=c(5,6), 
+                                        equiv_threshold = 0.2,
+                                        pretreatment_period = pre_treatment_period, 
+                                        base_period = base_period,
+                                        cluster = 7, data = sim_data,
+                                        alpha = alpha,
+                                        type = "IU")
 
+  expect_snapshot(print(maxEquivTest_results4))
+  
+})
 
 
 
